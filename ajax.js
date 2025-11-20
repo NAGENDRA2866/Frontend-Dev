3855129702ade01ac298b23b463e6db5c6c7317e
NAGENDRA2866 <mirthipatinagendra09@gmail.com>
2025-11-20 11:30:40 +0530

    added files

---
+++ ajax.js
@@ -0,0 +1,74 @@
+function getDataf() {
+    let fullname = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
+    let password = document.getElementById('password').value;
+    // let confirmPassword = document.getElementById('Confirmpassword').value;
+    let phone = document.getElementById('phone').value;
+
+    console.log("FullName: " + fullname);
+    console.log("phone: " + phone);
+    console.log("Email: " + email + "Password: " + password);
+    // alert("Email: " + email +"\n" + "Password: " + password + "\n" + "Phone: " + phone);
+
+    let emailPattern = /[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+    let passwordPattern = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{7,15}$/;
+    let phonePattern = /^[0-9]{10}$/;
+
+    if (fullname === "" || email === "" || password === "" || phone === "") {
+        alert("Please fill all the fields");
+    }
+    else if (fullname.length < 3) {
+        alert("Full name must be at least 3 characters");
+    }
+    else if (!email.includes("@")) {
+        alert("Please enter a valid email");
+    }
+    else if (!emailPattern.test(email)) {
+        alert("Please enter a valid email");
+    }
+    else if (!passwordPattern.test(password)) {
+        alert("Password must be 7-15 characters long and include at least one numeric digit and a special character");
+    }
+    else if (!phone.match(phonePattern)) {
+        alert("Phone number must be exactly 10 digits");
+    }
+    else {
+        createPost(fullname, phone, password, country);
+    }
+}
+
+// // Base API URL
+// const API_URL = "https://jsonplaceholder.typicode.com/posts";
+// // GET Request - Retrieve Data
+// function getPosts() {
+// fetch(API_URL)
+// .then(response => response.json())
+// .then(data => {
+// console.log("GET → All Posts:", data);
+// })
+// .catch(error => console.error("GET Error:", error));
+// }
+
+function createPost(fullname, Phone, password, country) {
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", "http://localhost:3000/signUp");
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.onload = function () {
+        console.log("Response:", JSON.parse(xhr.responseText));
+    };
+    const body = {
+        fullname: fullname,
+        Phone: Phone,
+        password: password,
+        // gender:gender,
+        // id:id,
+        country: country
+    };
+    xhr.send(JSON.stringify(body));
+}
+
+document.getElementById('button').addEventListener('click', (e) => {
+    e.preventDefault();
+    getDataf();
+
+});
---
+++ fetch.js
@@ -0,0 +1,81 @@
+function getDataf(){
+    let fullname = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
+    let password = document.getElementById('password').value;
+    // let confirmPassword = document.getElementById('Confirmpassword').value;
+    let phone = document.getElementById('phone').value;
+
+    console.log("FullName: " + fullname);
+    console.log("phone: " + phone);
+    console.log("Email: " + email + "Password: " + password);
+    // alert("Email: " + email +"\n" + "Password: " + password + "\n" + "Phone: " + phone);
+
+    let emailPattern = /[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+    let passwordPattern = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{7,15}$/;
+    let phonePattern = /^[0-9]{10}$/;
+
+    if (fullname==="" || email === "" || password === "" || phone === "")
+    {
+        alert("Please fill all the fields");
+    }
+    else if(fullname.length < 3)
+    {
+        alert("Full name must be at least 3 characters");
+    }
+    else if(!email.includes("@"))
+    {
+        alert("Please enter a valid email");
+    }
+    else if (!emailPattern.test(email)) {
+        alert("Please enter a valid email");
+    } 
+    else if (!passwordPattern.test(password)) {
+        alert("Password must be 7-15 characters long and include at least one numeric digit and a special character");
+    }
+    else if(!phone.match(phonePattern))
+    {
+        alert("Phone number must be exactly 10 digits");
+    }
+    else
+    {
+        createPost(fullname, phone, password,country);
+    }
+}
+
+// // Base API URL
+// const API_URL = "https://jsonplaceholder.typicode.com/posts";
+// // GET Request - Retrieve Data
+// function getPosts() {
+// fetch(API_URL)
+// .then(response => response.json())
+// .then(data => {
+// console.log("GET → All Posts:", data);
+// })
+// .catch(error => console.error("GET Error:", error));
+// }
+
+function createPost(fullname,Phone,password,country) {
+fetch('http://localhost:3000/signUp', {
+method: "POST",
+headers: { "Content-Type": "application/json" },
+body: JSON.stringify({
+    fullname:fullname,
+    Phone:Phone,
+    password:password,
+    // gender:gender,
+    // id:id,
+    country:country
+})
+})
+.then(response => response.json())
+.then(data => {
+console.log("POST → Created Post:", data);
+})
+.catch(error => console.error("POST Error:", error));
+}
+
+document.getElementById('button').addEventListener('click', (e) => {
+    e.preventDefault();
+    getDataf();
+    
+});
